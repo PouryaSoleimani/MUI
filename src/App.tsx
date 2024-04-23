c9b35d4ee00c8463011faa29eec6989a9a1ae8ba
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-04-23 15:59:37 -0700

    First init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-blue-700 py-4 text-center font-extrabold text-4xl border-b-8 border-black text-white tracking-wider ">MUI</h1>
+    </>
+  )
+}
+
+export default App
