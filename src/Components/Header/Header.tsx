eaa256f30687e5a6f55e27684100f40e67efd62d
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-04-24 06:23:17 -0700

    cofiging Header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,8 @@
 
-
 const Header = () => {
-    return (
-        <div className='w-screen'>
-            Header
-        </div>
-    )
+  return (
+    <div className="w-screen text-center bg-black py-4 text-white text-4xl">Header</div>
+  )
 }
 
 export default Header
