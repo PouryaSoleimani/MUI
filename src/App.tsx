919e5f36439b6abd59f2c0875c3070d008d0b03a
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-04-24 03:03:28 -0700

    Adding SideBar Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import Typography from "@mui/material/Typography"
-import Button from '@mui/material/Button';
-import { IconButton, Stack } from "@mui/material";
+import Navbar from "./Components/Navbar/Navbar"
+
 
 function App() {
 
   return (
     <>
-      <h1 className="bg-blue-700 py-4 text-center font-extrabold text-4xl border-b-8 border-black text-white tracking-wider ">MUI</h1>
-
-      <Typography variant="h3" component="h3" align="center" className="text-red-700" mt={10} fontWeight={800}>h1. Heading</Typography>
-      <Stack spacing={2} direction="row" className="mx-auto flex justify-center items-center">
-        <Button variant="contained" color="success" size="large" startIcon endIcon>Submit</Button>
-        <IconButton></IconButton>
-      </Stack>
+      <Navbar />
     </>
   )
 }
