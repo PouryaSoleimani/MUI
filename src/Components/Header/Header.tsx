9b7082a70acd0be002a2f3414e561e97e49378bd
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-04-24 06:14:37 -0700

    Adding Header

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div className='w-screen'>
+            Header
+        </div>
+    )
+}
+
+export default Header
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,48 +1,64 @@
-//^ NAVBAR COMPONENT
+// ^ NAVBAR COMPONENT
 import Drawer from '@mui/material/Drawer';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-
-
-
-
+// & ICONS
+import { Archive, Bot, Database, FileQuestion, FlaskConical, Gauge, LineChart, Parentheses, ScreenShare, Settings, User, UserPlus } from 'lucide-react';
 
 
 const Navbar = () => {
-    const drawerWidth = 200
+    const drawerWidth = 220;
 
     return (
-        <div>
-            <Drawer sx={{ width: drawerWidth, flexShrink: 0, '& .MuiDrawer-paper': { width: drawerWidth, boxSizing: 'border-box', }, }} variant="permanent" anchor="left"  >
-                <Toolbar />
+        <div >
+
+            <Drawer sx={{ width: drawerWidth, height: "100%", flexShrink: 0, '& .MuiDrawer-paper': { width: drawerWidth, boxSizing: 'border-box', }, }} variant="permanent" anchor="left" >
+                <h1 className='bg-black px-2 py-4 text-white font-bold text-2xl'>DASHBOARD</h1>
                 <Divider />
-                <List>
-                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem key={text} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                </ListItemIcon>
+                <List className='bg-zinc-800 text-white h-1/3'>
+                    {['Authentication', 'Database', 'Storage', 'Hosting', 'Functions', 'Machine Learning',].map((text, index) => (
+                        <ListItem key={text} disablePadding className='hover:bg-black'>
+                            <ListItemButton className='flex space-x-2 font-extrabold'>
+                                {index === 0 && <User strokeWidth={2.25} />}
+                                {index === 1 && <Database strokeWidth={2.25} />}
+                                {index === 2 && <Archive strokeWidth={2.25} />}
+                                {index === 3 && <ScreenShare strokeWidth={2.25} />}
+                                {index === 4 && <Parentheses strokeWidth={2.25} />}
+                                {index === 5 && <Bot strokeWidth={2.25} />}
                                 <ListItemText primary={text} />
                             </ListItemButton>
                         </ListItem>
                     ))}
                 </List>
                 <Divider />
-                <List>
-                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                        <ListItem key={text} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                                </ListItemIcon>
+                <List className='bg-zinc-800 text-white font-bold h-1/3'>
+                    {['Analytics', 'Performance', 'Test Lab', 'Settings', 'Sign Up / Login', 'FAQ', 'DOCS'].map((text, index) => (
+                        <ListItem key={text} disablePadding className='hover:bg-black'>
+                            <ListItemButton className='flex space-x-2 font-extrabold'>
+                                {index === 0 && <LineChart strokeWidth={2.25} />}
+                                {index === 1 && <Gauge strokeWidth={2.25} />}
+                                {index === 2 && <FlaskConical strokeWidth={2.25} />}
+                                {index === 3 && <Settings strokeWidth={2.25} />}
+                                {index === 4 && <UserPlus strokeWidth={2.25} />}
+                                {index === 5 && <FileQuestion strokeWidth={2.25} />}
+                                <ListItemText primary={text} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider />
+                <List className='bg-zinc-800 text-white font-bold h-1/3'>
+                    {['Analytics', 'Performance', 'Test Lab', 'Settings'].map((text, index) => (
+                        <ListItem key={text} disablePadding className='hover:bg-black'>
+                            <ListItemButton className='flex space-x-2 font-extrabold'>
+                                {index === 0 && <User strokeWidth={2.25} />}
+                                {index === 1 && <Database strokeWidth={2.25} />}
+                                {index === 2 && <Archive strokeWidth={2.25} />}
+                                {index === 3 && <ScreenShare strokeWidth={2.25} />}
                                 <ListItemText primary={text} />
                             </ListItemButton>
                         </ListItem>
