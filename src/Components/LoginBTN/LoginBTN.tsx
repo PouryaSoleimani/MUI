d1e62e284b9e7897f8f0e7a6c2441dc34e9fd8d5
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-04-26 04:18:30 -0700

    ...

--- src/Components/LoginBTN/LoginBTN.tsx
+++ src/Components/LoginBTN/LoginBTN.tsx
@@ -27,7 +27,7 @@ export default function LoginBTN() {
                             </ModalFooter>
                         </>
                     )}
-                </ModalContent> 
+                </ModalContent>
             </Modal>
         </>
     );
