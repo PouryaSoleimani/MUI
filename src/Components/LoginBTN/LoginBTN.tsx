8302aec5049f73616906e44bbbe0da7f13ffa2c5
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-04-26 04:16:33 -0700

    ...

--- src/Components/LoginBTN/LoginBTN.tsx
+++ src/Components/LoginBTN/LoginBTN.tsx
@@ -9,12 +9,10 @@ export default function LoginBTN() {
         <>
             <Button onPress={onOpen} color="primary" className="font-bold text-lg">SignUp / Login</Button>
             <Modal backdrop="blur" isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
-
                 <ModalContent className="bg-zinc-900 flex flex-col text-2xl text-zinc-100 font-bold rounded-xl">
                     {(onClose) => (
                         <>
                             <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
-
                             <ModalBody>
                                 <Input autoFocus endContent={<Mail className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />} placeholder="Enter your email" variant="bordered" className="text-lg border-2 p-1 rounded-xl border-black " />
                                 <Input endContent={<Lock className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />} placeholder="Enter your password" type="password" variant="bordered" className="text-lg border-2 border-black p-1 rounded-xl" />
@@ -23,16 +21,13 @@ export default function LoginBTN() {
                                     <Link color="primary" href="#" size="md"> Forgot password?</Link>
                                 </div>
                             </ModalBody>
-
                             <ModalFooter>
                                 <Button className="bg-rose-600/70 text-white font-semibold h-fit p-4 text-md  tracking-tighter rounded-xl" onPress={onClose}> Close</Button>
                                 <Button className="bg-emerald-600/70 text-white font-semibold p-4 h-fit text-md  tracking-tighter rounded-xl" onPress={onClose}> SignUp</Button>
-
                             </ModalFooter>
                         </>
                     )}
-                </ModalContent>
-
+                </ModalContent> 
             </Modal>
         </>
     );
